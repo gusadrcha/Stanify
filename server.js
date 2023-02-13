76413359a7801372a4062e0d959a6e4179432b63
Gustavo <chavezgustavo813@gmail.com>
2023-02-12 20:04:50 -0600

    imported necessary libraries into the server

---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Environment Variables
+const PORT = process.env.PORT;
+
+// Libraries
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const bodyParser = require('body-parser');
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Routes
+//app.use('/Spotify', require('./routes/Spotify'))
+
+// View Engine
+app.set('views', './views')
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { artistName: '' });
+})
+
+// Start up the server
+app.listen(PORT, () =>{
+    console.log("Welcome to the server");
+})
