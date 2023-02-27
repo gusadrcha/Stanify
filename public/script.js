59de50cd53c208d4c9e07895f093f0af956574c5
Arzenith <patrick.arzou@gmail.com>
2023-02-27 14:01:34 -0600

    Limit Possible song choices to 4, Navbar + Font changes

--- public/script.js
+++ public/script.js
@@ -261,10 +261,11 @@ guessInput.addEventListener('input', function(e) {
     // Append the DIV element as a child of the autocomplete container:
     this.parentNode.appendChild(possibleSongDiv);
 
+    var possible_song_count = 0;
     // For each item in the array...
     for (i = 0; i < songNames.length; i++) {
         // Check if the item starts with the same letters as the text field value:
-        if (songNames[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        if (songNames[i].substr(0, val.length).toUpperCase() == val.toUpperCase() && possible_song_count <= 3) {
             // Create a DIV element for each matching element:
             possibleSongItem = document.createElement("DIV");
             // Make the matching letters bold:
@@ -282,6 +283,8 @@ guessInput.addEventListener('input', function(e) {
                 closeAllLists();
             });
             possibleSongDiv.appendChild(possibleSongItem);
+
+            possible_song_count += 1;
         }
     }
 });
